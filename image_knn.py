230bae3e7fa224198213297ac47713a86c037c0a
Zhenguo <Zhenguo.Chen@colorado.edu>
2017-04-06 17:12:56 -0600

    add bleu score computation

--- image_knn.py
+++ image_knn.py
@@ -2,13 +2,17 @@
 knn model for image description
 '''
 from DataReader import DataSet
+from utils import consensus
+from utils import accuracy
 from sklearn.neighbors import BallTree
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+from nltk.translate import bleu_score
 
 banner1 = "*" *10
 banner2 = "-" *10
 banner3 = "&" *5
+
 class KNN:
     '''
     knn model for generating description for images
@@ -30,26 +34,22 @@ class KNN:
         :return:
         '''
         dst, ind = self.knn.query(examples, self.k)
+
+        consensus_captions = [] #consensus captions
+
         for i, ids in enumerate(ind):
             print 'predict image ', i
+            captions = []  # candidate captions
             for j, id in enumerate(ids):
-                print 'closed image:', j, 'image id:', id
+                # print 'closed image:', j, 'image id:', id
                 sentences = self.trains['descriptions'][id]['sentences']
                 for sentence in sentences:
-                    vals =  sentence['raw']
-        if len(vals) <=1:
-            print vals
-            
-    def majority(self, ind):
-        """given a list of indicies return the majority label"""
-        for i, ids in enumerate(ind):
-            print banner1, 'predict image ', i, banner1
-            for j, id in enumerate(ids):
-                print banner2, 'closed image:', j, 'image id:', id, banner2
-                tokens = self.trains['descriptions'][id]['tokens']
-                sentences = self.trains['descriptions'][id]['sentences']
-                for sentence in sentences:
-                    print sentence['raw']
+                    captions.append(sentence['raw'])
+
+            consensus_captions.append(consensus(captions))
+
+        return consensus_captions
+
 
     def eval(self, examples, descriptions):
         '''
@@ -57,22 +57,80 @@ class KNN:
         :return:
         '''
         dst, ind = self.knn.query(examples, self.k)
-        #choice = self.majority(self, ind)
-        #print("best sentence {}".format(descriptions[choice]))
+
+        consensus_captions = []  # consensus captions
+        actual_descriptions = []    # actual descriptions for all examples
+
         for i, ids in enumerate(ind):
             print('{0} predict image {1} filename: {2} {0}'.format(banner1, i, descriptions[i]['filename']))
+            captions = []  # candidate captions
             for j, id in enumerate(ids):
+
+                '''
                 print('{0} closed image: {1} image id: {2} {0}'.format(banner2, j, id))
                 sentences = self.trains['descriptions'][id]['sentences']
                 for sentence in sentences:
                     print sentence['raw']
-            sentences =  descriptions[i]["sentences"]
+                '''
+                #get consensus captions
+                sentences = self.trains['descriptions'][id]['sentences']
+                for sentence in sentences:
+                    captions.append(sentence['raw'])
+
+            print consensus(captions)
+            consensus_captions.append(consensus(captions))
+
+            sentences = descriptions[i]["sentences"]
+            print('actual description:')
+            actual_description = []
             for sentence in sentences:
-                print("{0} actual description: \n {1}\n {0}".format(banner3, sentence['raw']))
+                actual_description.append(sentence['raw'])
+                print(sentence['raw'])
+            actual_descriptions.append(actual_description)
+
             img = mpimg.imread('data/Flicker8k_Dataset/'+descriptions[i]['filename'])
-            print 'data/Flicker8k_Dataset/'+descriptions[i]['filename']
             plt.imshow(img)
             plt.show()
+
+        return consensus_captions, actual_descriptions
+
+    def accuracy(self, examples, descriptions):
+        '''
+        using valid data to compute the accuracy of knn model
+        :param examples:
+        :param descriptions:
+        :return:
+        '''
+        dst, ind = self.knn.query(examples, self.k)
+
+        consensus_captions = []  # consensus captions
+        actual_descriptions = []  # actual descriptions for all examples
+
+        print 'computing accuracy'
+
+        for i, ids in enumerate(ind):
+            captions = []  # candidate captions
+            for j, id in enumerate(ids):
+
+                # get consensus captions
+                sentences = self.trains['descriptions'][id]['sentences']
+                for sentence in sentences:
+                    captions.append(sentence['raw'])
+
+            consensus_captions.append(consensus(captions))
+
+            sentences = descriptions[i]["sentences"]
+            actual_description = []
+            for sentence in sentences:
+                actual_description.append(sentence['raw'])
+            actual_descriptions.append(actual_description)
+
+            if i%10 == 0:
+                print "{0:.0f}%".format(float(i)/len(examples) * 100)
+
+        print '100%'
+
+        print accuracy(consensus_captions, actual_descriptions)
 
 
 if __name__ == '__main__':
@@ -80,5 +138,8 @@ if __name__ == '__main__':
     trains = data.get_trains()
     knn = KNN(trains)
     valids = data.get_valids()
-    knn.eval(valids['feats'][:10], valids['descriptions'][:10])
-    # knn.predict(valids['feats'][:10])
+    predict, actual = knn.eval(valids['feats'][:10], valids['descriptions'][:10])
+    predict = knn.predict(valids['feats'][:10])
+    #knn.accuracy(valids['feats'][:20], valids['descriptions'][:20])
+    acc = accuracy(predict, actual)
+    print acc
---
+++ utils.py
@@ -0,0 +1,39 @@
+'''
+common functions used by both knn model and Neural Network
+'''
+from nltk.translate import bleu_score
+
+def consensus(captions):
+    '''
+    get consensus caption
+    :param captions: candidate captions
+    :return: the consensus caption which maxmizes sum(sim(c,c')), for sim we use bleu score
+    '''
+    max = 0.0
+    consensus = captions[0]
+
+    # get the max bleu_score
+    for caption in captions:
+        references = captions[:]
+        references.remove(caption)
+        socre = bleu_score.sentence_bleu(references, caption)
+        if caption > max:
+            max = socre
+            consensus = caption
+
+    return consensus
+
+def accuracy(predict, real):
+    '''
+    use bleu score as a measurement of accuracy
+    :param predict: a list of predicted captions
+    :param real: a list of actual descriptions
+    :return: bleu accuracy
+    '''
+    accuracy = 0
+    for i, pre in enumerate(predict):
+        references = real[i]
+        score = bleu_score.sentence_bleu(references, pre)
+        accuracy += score
+
+    return accuracy/len(predict)
