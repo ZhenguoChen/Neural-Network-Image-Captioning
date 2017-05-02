31d4abd2a9c3b6a951fb2969722c2074bf8585c2
ZhenguoChen <Zhenguo.Chen@colorado.edu>
2017-05-02 04:51:30 -0600

    finalize vgg feature extractino, add vgg19 version

--- feature_extraction/FeatureExtractor_Keras.py
+++ feature_extraction/FeatureExtractor_Keras.py
@@ -12,8 +12,6 @@ import scipy.io
 def feature_extraction(image_path):
     model = VGG16(weights='imagenet', include_top=True)
 
-    img_path = 'image/dog.jpg'
-
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -22,7 +20,7 @@ def feature_extraction(image_path):
 
     featextractor_model = Model(input=model.input, outputs=model.get_layer('fc2').output)
     features = featextractor_model.predict(x)
-    print features
+    # print features
     return features
 
 def feature_extraction_VGG19(image_path):
