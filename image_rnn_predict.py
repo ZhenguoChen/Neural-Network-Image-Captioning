0a2ef8e18da0811c7ade2c0be35c569972e7dbbf
ZhenguoChen <Zhenguo.Chen@colorado.edu>
2017-05-02 18:13:56 -0600

    get features in batch, then do captioning

--- image_rnn_predict.py
+++ image_rnn_predict.py
@@ -1,5 +1,5 @@
 from image_rnn import Image_LSTM
-from feature_extraction.FeatureExtractor_Keras import feature_extraction
+from feature_extraction.FeatureExtractor_Keras import feature_extraction_batch
 
 def captioning(images):
     '''
@@ -8,19 +8,21 @@ def captioning(images):
     :return: captions
     '''
     model = Image_LSTM()
-    model.load_weights()
+    model.load_weights('model/keras/checkpoint_0.h5')
+    model.set_dict()
     captions = []
-    for img in images:
-        feat = feature_extraction(img)
+
+    feats = feature_extraction_batch()
+    for feat in feats:
         caption = model.predict(feat)
         captions.append(caption)
 
     print captions
 
 if __name__ == '__main__':
-    img1 = 'static/dog.jpg'
-    img2 = 'static/dog1.jpg'
-    img3 = 'static/dog2.jpg'
+    img1 = 'static/img/download.jpg'
+    img2 = 'static/img/dog1.jpg'
+    img3 = 'static/img/dog2.jpg'
 
     imgs = [img1, img2, img3]
 
